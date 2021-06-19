57476e583f6cee72a646d6e7fbe2c6a0996a0afc
GOD <masterionic@gmail.com>
2021-06-19 16:36:05 -0400

    update remove error page

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use('/', indexRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // // error handler
 // app.use(function(err, req, res, next) {
